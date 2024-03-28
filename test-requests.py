667f8a51ece639162ad77db3bf1b358b945e35db
Logan Lucas <logan.lucas83@gmail.com>
2024-03-28 14:51:01 +0100

    feat: python script to perform HTTP requests

---
+++ test-requests.py
@@ -0,0 +1,95 @@
+import requests
+
+token = None
+
+def print_help():
+    print(" --- Utils ---")
+    print("h - help")
+    print("q - quit")
+    print(" --- Authentication ---")
+    print("r - register user")
+    print("l - login as user")
+    print(" --- Authenticated requests ---")
+    print("hi - hello")
+    print("a - add report")
+    print()
+
+def user_choice():
+    choice = input("What would you like to do ? ")
+    if choice == "r":
+        register()
+    elif choice == "l":
+        login()
+    elif choice == "hi":
+        hello()
+    elif choice == "a":
+        add_report()
+    elif choice == "q":
+        exit()
+    else:
+        print_help()
+
+def register():
+    first_name = input("What is your first name ? ")
+    last_name = input("What is your last name ? ")
+    username = input("What is your username ? ")
+    password = input("What is your password ? ")
+    headers = {"Content-Type": "application/json"}
+    payload = {"firstName": first_name, "surname": last_name, "username":
+               username, "password": password}
+    r = requests.post("http://localhost:3333/auth/register", json=payload,
+                      headers=headers)
+    print(r.json())
+    print()
+
+def login():
+    global token
+
+    username = input("What is your username ? ")
+    password = input("What is your password ? ")
+    headers = {"Content-Type": "application/json"}
+    payload = {"username": username, "password": password}
+    r = requests.post("http://localhost:3333/auth/login", json=payload,
+                      headers=headers)
+    print(r.json())
+    if r.ok:
+        token = r.json()["token"]
+    print()
+
+def hello():
+    if not token == None:
+        headers = {"Authorization": "Bearer " + token}
+        r = requests.get("http://localhost:3333/hello", headers=headers)
+        print(r.json())
+        print()
+
+def add_report():
+    if not token == None:
+        title = input("What is the title of your report ? ")
+        is_blockage = input("Is your report a blockage ? ")
+        address = input("What is the address of your report ? ")
+        city = input("What is the city of your report ? ")
+        zip_code = input("What is the zip code of your city ? ")
+        description = input("What is the description of your report ? ")
+        headers = {"Authorization": "Bearer " + token,
+                   "Content-Type": "application/json"}
+        payload = {
+            "title": title,
+            "isBlockage": is_blockage,
+            "address": address,
+            "city": city,
+            "zipCode": zip_code,
+            "description": description
+        }
+        r = requests.post("http://localhost:3333/add-report", json=payload,
+                          headers=headers)
+        print(r.json())
+    return
+
+def main():
+    print_help()
+    while True:
+        user_choice()
+
+if __name__ == "__main__":
+    main()
