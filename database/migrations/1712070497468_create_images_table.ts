1157b4e2e97a40151a7c463ee64162702c01ba09
Logan Lucas <logan.lucas83@gmail.com>
2024-04-07 22:03:34 +0200

    chore: forgot something

--- database/migrations/1712070497468_create_images_table.ts
+++ database/migrations/1712070497468_create_images_table.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
     async up() {
         this.schema.createTable(this.tableName, (table) => {
-            table.increments("id");
+            table.increments("id").primary();
             table.string("file_name").notNullable();
             table.integer("report_id").unsigned().references("reports.id").onDelete("CASCADE");
 
